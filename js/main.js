04efbbb75ba4e606aad2b3b3ef22fcf1f99d56fb
Simão Gato <simao.valedegato@gmail.com>
2024-07-12 18:13:52 +0100

    style: add dark mode support

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-// Sample script for future interactivity
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('JavaScript loaded and ready.');
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleIcon = document.getElementById('background-mode');
+
+    const setThemeAndIcon = (theme) => {
+        document.documentElement.setAttribute('data-applied-mode', theme);
+        localStorage.setItem('theme', theme);
+        toggleIcon.src = theme === 'light' ? 'icons/moon.png' : 'icons/sun.png';
+        toggleIcon.alt = theme === 'light' ? 'Moon Icon' : 'Sun Icon';
+    };
+
+    // Set the initial icon based on the saved theme
+    const initialTheme = document.documentElement.getAttribute('data-applied-mode');
+    setThemeAndIcon(initialTheme);
+
+    toggleIcon.addEventListener('click', () => {
+        const currentTheme = document.documentElement.getAttribute('data-applied-mode');
+        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+        setThemeAndIcon(newTheme);
+    });
 });
 
